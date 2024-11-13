c776ffd070406c42b5672a2664a631d0af3701bd
waqaskhan132 <waqaskhan@gmail.com>
2024-11-13 03:24:17 -0800

    Added Model classes and sql setting in setting.py

--- cousre7_little_lemon/cousre7_little_lemon/settings.py
+++ cousre7_little_lemon/cousre7_little_lemon/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'little_lemon',
 ]
 
 MIDDLEWARE = [
@@ -74,10 +75,17 @@ WSGI_APPLICATION = 'cousre7_little_lemon.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': {  
+        'ENGINE': 'django.db.backends.mysql',  
+        'NAME': 'little_09',  
+        'USER': 'root',  
+        'PASSWORD': 'Toor',  
+        'HOST': '127.0.0.1',  
+        'PORT': '3306',  
+        'OPTIONS': {  
+            'init_command': "SET sql_mode='STRICT_TRANS_TABLES'"  
+        }  
+    }  
 }
 
 
